0475aa7f97e62d2b64daf6a2d0cc2fd864691e20
Harald Rudell <harald.rudell@gmail.com>
2018-12-09 23:46:20 -0800

    Updates

---
+++ precache-manifest.2d3568bc0e5c8206cd131e0d7e2061c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c9784151404f2112cd8",
+    "url": "/react-fame/static/css/main.d67bbded.chunk.css"
+  },
+  {
+    "revision": "8c9784151404f2112cd8",
+    "url": "/react-fame/static/js/main.8c978415.chunk.js"
+  },
+  {
+    "revision": "672f6e57b7e3eb2426d8",
+    "url": "/react-fame/static/js/1.672f6e57.chunk.js"
+  },
+  {
+    "revision": "98010df4823187c66a46",
+    "url": "/react-fame/static/js/runtime~main.98010df4.js"
+  },
+  {
+    "revision": "00c8e4b0297bdb103d2e3ead977dd5bf",
+    "url": "/react-fame/static/media/Blade+Runner.00c8e4b0.ttf"
+  },
+  {
+    "revision": "b0f65282af3ce1b299dbccc3853f2fe0",
+    "url": "/react-fame/index.html"
+  }
+];
