d020229ded019de8bebde88b898b038dda579dfb
Harald Rudell <harald.rudell@gmail.com>
2018-12-20 22:13:55 -0800

    back to the future: react 16.7.0-alpha.2

--- src/App.test.js
+++ src/App.test.js
@@ -3,11 +3,11 @@
 This source code is licensed under the ISC-style license found in the LICENSE file in the root directory of this source tree.
 */
 import React from 'react'
-//import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom'
 import App from './App'
-import renderer from 'react-test-renderer'
 
 it('renders without crashing', () => {
-  const tree = renderer.create(<App />).toJSON()
-  expect(tree).toMatchSnapshot()
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+  ReactDOM.unmountComponentAtNode(div)
 })
