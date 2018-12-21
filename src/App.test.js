4a0c725c7cc1d2175ba9625b84a5b6765d98fb30
Harald Rudell <harald.rudell@gmail.com>
2018-12-20 21:22:40 -0800

    trying to  ake testing work

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+/*
+© 2018-present Harald Rudell <harald.rudell@gmail.com> (http://www.haraldrudell.com)
+This source code is licensed under the ISC-style license found in the LICENSE file in the root directory of this source tree.
+*/
+import React from 'react'
+//import ReactDOM from 'react-dom'
+import App from './App'
+import renderer from 'react-test-renderer'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const tree = renderer.create(<App />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
